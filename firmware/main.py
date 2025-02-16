953f42ec02de82ad9582892b27c4e26eff32ef77
pakkid <160797397+pakkid@users.noreply.github.com>
2025-02-17 07:42:57 +1300

    Add MCP23017 support and update encoder configuration

--- firmware/main.py
+++ firmware/main.py
@@ -2,6 +2,7 @@
 import board
 import busio
 # These are imports from the kmk library
+from adafruit_mcp230xx.mcp23017 import MCP23017
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
@@ -10,24 +11,29 @@ from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.extensions.media_keys import MediaKeys
 from kmk.handlers.sequences import simple_key_sequence
 
-# This is the main instance of your keyboard
+# Keyboard stuff
 i2c = busio.I2C(scl=board.SCL, sda=board.SDA, frequency=100000)
 mcp = MCP23017(i2c, address=0x20)
 keyboard = KMKKeyboard()
 
+# Add the media keys extension
+keyboard.extensions.append(MediaKeys())
+
 # Encoder Configuration
-encoder_handler.pins = ((mcp.get_pin(1), mcp.get_pin(0), mcp.get_pin(2)), ((mcp.get_pin(3), mcp.get_pin4), mcp.get_pin(5)))  # Define pins for encoders
+encoder_handler = EncoderHandler()
+encoder_handler.pins = ((mcp.get_pin(9), mcp.get_pin(10), mcp.get_pin(8)), (mcp.get_pin(12), mcp.get_pin(13), mcp.get_pin(11)))  # Define pins for encoders
 encoder_handler.map = [
-    ((KC.VOLU, KC.VOLD, KC.MUTE)),  # Encoder 1 - Volume Control
-    ((KC.LEFT, KC.RIGHT, KC.LALT(KC.TAB)))  # Encoder 2 - App Switching
+    (KC.VOLU, KC.VOLD, KC.MUTE),  # Encoder 1 - Volume Control
+    (KC.LCTRL(KC.MINUS), KC.LCTRL(KC.EQUAL), KC.NO)  # Encoder 2 - App Switching
 ]
+keyboard.modules.append(encoder_handler)
 
 # Add the macro extension
 macros = Macros()
 keyboard.modules.append(macros)
 
-# Define your pins here!
-PINS = [board.D3, board.D4, board.D2, board.D1]
+# my pins
+PINS = [board.D10, board.D9, board.D8, board.D7, board.D3, mcp.get_pin(0), mcp.get_pin(15), mcp.get_pin(14), mcp.get_pin(2), mcp.get_pin(1), mcp.get_pin(4), mcp.get_pin(3), mcp.get_pin(6), mcp.get_pin(5)]
 
 # Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
@@ -36,26 +42,20 @@ keyboard.matrix = KeysScanner(
 )
 
 OPEN_VSCODE = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("code"), KC.ENTER))
-OPEN_GITHUB = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("github-desktop"), KC.ENTER))
+OPEN_GITHUB = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("github"), KC.ENTER))
 OPEN_TERMIUS = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("termius"), KC.ENTER))
 OPEN_KICAD = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("kicad"), KC.ENTER))
 OPEN_FUSION360 = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("fusion360"), KC.ENTER))
 OPEN_STEAM = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("steam"), KC.ENTER))
 OPEN_DISCORD = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("discord"), KC.ENTER))
-OPEN_NOTEPAD = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("notepad-plus-plus"), KC.ENTER))
+OPEN_NOTEPAD = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("notepad++"), KC.ENTER))
 OPEN_VMWARE = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("vmware"), KC.ENTER))
-OPEN_CHROME = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("google-chrome"), KC.ENTER))
+OPEN_CHROME = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("chrome"), KC.ENTER))
 OPEN_SPOTIFY = simple_key_sequence((KC.LGUI(KC.R), KC.MACRO_SLEEP_MS(100), KC.COPY("spotify"), KC.ENTER))
 
-
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+# silly keymap
 keyboard.keymap = [
-    []
-    []
-    []
-    []
+    [KC.MPRV, KC.MPLY, KC.MNXT, OPEN_VSCODE, OPEN_GITHUB, OPEN_TERMIUS, OPEN_KICAD, OPEN_FUSION360, OPEN_STEAM, OPEN_DISCORD, OPEN_NOTEPAD, OPEN_VMWARE, OPEN_CHROME, OPEN_SPOTIFY]
 ]
 
 # Start kmk!
